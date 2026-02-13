237c326147730f8087f7e934df057c74dfc567fc
Neuromancer <m.arda.safak06@gmail.com>
2026-02-13 23:40:26 +0300

    This time at least this works

--- primePrinter.c
+++ primePrinter.c
@@ -2,37 +2,35 @@
 #include <math.h>
 #include <limits.h>
 
-int main(void)
-{
-	int count;
-	printf("How many primes you want to see starting from 2:");
-	fflush(stdout);
-	scanf("%d",&count);
-	int isPrime = 1;
-	int iter = 0;
-	printf("2\n");
-	for(int i = 3;i < INT_MAX;i++)
-	{
-		for (int j = 2;j < ceil(sqrt((double) j)) ;j++)
-		{
-			if (i % j == 0)
-			{
-				isPrime = 0;
-				break;
-			}
-		}	
-		if (isPrime)
-		{
-			printf("%d\n",i);
-			iter++;
-		}
-		
-		if(iter == count-1)
-		{
-			break;
-		}	
-		i++;
-	}
-	return(0);
+int main(void) {
+    int count;
+    printf("How many primes do you want starting from 2? ");
+    if (scanf("%d", &count) != 1 || count < 1) {
+        printf("Bro enter a positive number, come on...\n");
+        return 1;
+    }
 
+    printf("2\n");
+    int found = 1;           
+
+    for (int i = 3; found < count; i += 2) 
+	{   
+        int is_prime = 1;
+        int sqrt_i = (int)ceil(sqrt((double)i));
+
+        for (int j = 2; j < sqrt_i; j++) 
+		{     
+            if (i % j == 0) {
+                is_prime = 0;
+                break;
+            }
+        }
+
+        if (is_prime) {
+            printf("%d\n", i);
+            found++;
+        }
+    }
+
+    return 0;
 }
