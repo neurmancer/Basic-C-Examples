1e7cc3693a6d970deceebb3aa3f16f26f88ffc09
neurmancer <m.arda.safak06@gmail.com>
2026-06-19 17:22:30 +0300

    Lol _start

--- reallyBasicThings/projects101/mainless.c
+++ reallyBasicThings/projects101/mainless.c
@@ -10,11 +10,12 @@
     2- _exit just exists immediately unlike return() which clears stdio buffers and shit you can use atexit() with a function to clear buffers with:
         - fflush(stdio) for output
         and a basic getchar loop for stdin buffer clear 
-
+    3- We're basically saying linker to fuck crt0.o itself (crt0.o is basically the 'safe' exit option that main returns to and 
+    init part where argc and argv get parsed so if you were to use cli arguments you gotta parse it manuelly 
+    if you use _start and if you feel like those words are gibbrish don't worry they are don't mind) 
     And totally unrelated as I search for difference between return and _exit google's 'Also check' thingy showed me a bullet point 
     suggesting 'How to code 'I Love you' in C' some people trying impress nerds ig....
-    be carefull gang don't fall for it unless it's a mainless I love you statment 
-
+    be carefull gang don't fall for it unless it's a mainless I love you statement 
 */
 
 int _start() {
@@ -23,5 +24,6 @@ int _start() {
     printf("Here is a prime number for you before I dip: 13\n");
     
     // Terminate the process immediately
-    _exit(0); 
+    _exit(0); //Basically a magnum to the temple with an empty bottle o' jack on the table (or kill -9 if you know)
+    //return(0); would cause segfault (if not UB) since there is no crt0.o return TO
 }
