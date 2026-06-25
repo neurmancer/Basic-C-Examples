e63324d0f28700f8a8d7e751a43becbce689021a
neurmancer <m.arda.safak06@gmail.com>
2026-06-26 01:36:53 +0300

    Failed kernel thingy

--- reallyBasicThings/projects101/lowLevelStuff/kernelStuff/notWorking/kernelDriverIntroThingy.c
+++ reallyBasicThings/projects101/lowLevelStuff/kernelStuff/notWorking/kernelDriverIntroThingy.c
@@ -1,10 +1,13 @@
 #include <linux/module.h> //   needed for all modules (they say)
 #include <linux/kernel.h> //Needed for KERN_INFO 
 
+
 /*  And if you can't tell this is a Linux only Lecture...duh*/
 
-
 /*
+            This TUTORIAL IS OUT-DATED AS FUCK AND DOESN'T WORK WITH THE GIVEN MAKFILE SO PLESE DON'T WASTE YOUR TIME I'LL LEAVE IT AS A ATTEMPT IN THE FOLDER
+
+
     Wassup? Dear cc, today we're gonna build the baby's first kernel module are you as exicted as me?(Satire...don't take this too seriously since neither do I)
     You know the tea...I saw something, I gotta dig...I lowkey feel like Sysphius...Push the boulder, find a new one at the bottom and push it again...
 
@@ -17,9 +20,14 @@
             And that's exactly how a documentation should start...it must acknowledge that you are new here and calm you down before getting techical anyways enough yap...         
     
 
+            This file probably will not work since the tutorial seems out-dated but we'll see
+
 */
 
-int init_module(void) //Doesn't take any argument 
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Test?");
+
+int my_module(void) //Doesn't take any argument 
 {
 	//What init does basically (man...it's hard to simplify KERNEL STUFF) registering a handler for something on the (or with the) kernel or 
 	//replace/overwrite one of kernel's own functions
@@ -28,8 +36,6 @@ int init_module(void) //Doesn't take any argument
 	printk(KERN_INFO "Hello, Ring 0 (still single?)\n");	//I'll explain the kernel print 'log priority levels' but as you can guess printk was not meant 
 								//to communicate with user directly...yk...that's the whole deal of...yk...being bare-metal  
 
-	
-	
 
 
 	return(0); //Returns something than 0 different on errors
@@ -43,3 +49,4 @@ void cleanup_module(void)
 {
 	printk(KERN_INFO "Now I'll just say goodbyeeee\n");
 }
+
