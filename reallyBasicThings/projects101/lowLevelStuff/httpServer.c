cea6fc758d29232dde20f18db88628e092755e09
neurmancer <m.arda.safak06@gmail.com>
2026-06-26 17:03:14 +0300

    Last commit before I delete stolen example from man 2 bind

--- reallyBasicThings/projects101/lowLevelStuff/httpServer.c
+++ reallyBasicThings/projects101/lowLevelStuff/httpServer.c
@@ -1,6 +1,11 @@
+#include <netinet/in.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
+
+//These ones here because Beej said so (and probably to get getaddrinfo())
+#include <netdb.h> 
+#include <sys/types.h>
 
 /*
     And here we are...I am diving head-first into networking as I code a fucking HTTP server...no idea what a HTTP server is for now...but I'll eventually learn on the way lol
@@ -27,19 +32,46 @@ int main(void)
             
             1- AF_INET is IPv4 domain define in the socket.h (at least man says lol...I'm still Terry Davis'ing my way to knowledge)
             2- SOCK_STREAM is the one we use for TCP protocal 
+
+
+            3- Man I am fucking confues AF...Beej's guide shows sin_family assignemnts... my cc -E source shows sa_family and the man example down below shows sun_family for the structs...
+
+                Guess(may be way off) sa is short for socket address sin is for socket_internet? and sun for socket unix family...fuck...
         
     */
-    int fd = socket(AF_INET,SOCK_STREAM,0 ); //fd's short for file descriptor 
+    int fd = socket(AF_INET,SOCK_STREAM,0 ); //fd's short for file descriptor  0 and IPPROTO_TCP for tcp(7) stream sockets that explains why I have 0 as protocol it is TCP as I want
     if(fd == -1) {perror("Socket, sucked it"); return(-1); }
     
-    struct sockaddr serverAdd = { 0 };
+    /*
+             .sin_family is always set to AF_INET.  This is required; in Linux 2.2 most networking functions return EINVAL when this setting is missing.  .sin_port contains the port in network  byte
+        order.   The  port numbers below 1024 are called privileged ports (or sometimes: reserved ports).  Only a privileged process (on Linux: a process that has the CAP_NET_BIND_SERVICE capa‐
+        bility in the user namespace governing its network namespace) may bind(2) to these sockets.  Note that the raw IPv4 protocol as such has no concept of a port, they are implemented  only
+        by higher protocols like tcp(7) and udp(7).
+
+        .sin_addr  is  the IP host address.  The .s_addr member of the in_addr(3type) structure contains the host interface address in network byte order.  in_addr(3type) should be assigned one
+        of the INADDR_* values (e.g., INADDR_LOOPBACK) using htonl(3) or set using the inet_aton(3), inet_addr(3), inet_makeaddr(3) library functions or directly with  the  name  resolver  (see
+        gethostbyname(3)).
     
-    serverAdd.sa_family = AF_INET;
+        Am I using wrong family for binding&naming
     
+
+    */
+
+
+
+
+    struct addrinfo hint = { 0 }; //They use memset for somereason but I'll stick with this 'till I get seg-assult which honestly if they're only 0 initilizing I won't get since this is the same thing
+    struct addrinfo *result; //I tried sockaddr but getaddrinfo bitched about not using 'addrinfo' ergo, I am using addrinfo blindly
+    
+    hint.ai_family = AF_INET; //IPv4 
+    hint.ai_socktype = SOCK_STREAM; //TCP stream
+    hint.ai_flags = AI_PASSIVE;     //Beej says this is for 'fill my IP for me' 
+
     // bind(fd, const struct sockaddr *addr, socklen_t len) no clue what it does for now lol... 
-    
-    int bindStatus = bind(fd,(struct sockaddr *)&serverAdd,sizeof(serverAdd)); //   Upon successful completion, bind() shall return 0; otherwise, -1 shall be returned and errno set to indicate the error. (man bind page)
-    if (bindStatus == -1) {perror("Binding to the socket has failed\n"); return(-1); }
+    int res = getaddrinfo(NULL,"8080", &hint,&result); //8080 for non-administrive port (is non-administrive a word? I dunno you got the idea...I don't need to be admin to access basically )
+
+    int bindStatus = bind(fd, result->ai_addr,result->ai_addrlen); //   Upon successful completion, bind() shall return 0; otherwise, -1 shall be returned and errno set to indicate the error. (man bind page)
+    if (bindStatus == -1) {perror("Avoidant attachment issue, see a therapist ASAP\n\n"); return(-1); }
 
     //since almost every server runs repeatetly 
     while (1) {
