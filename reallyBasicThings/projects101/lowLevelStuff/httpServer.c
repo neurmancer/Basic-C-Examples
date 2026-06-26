df405fb1735f58491c2029a0680e2eb25d3498d0
neurmancer <m.arda.safak06@gmail.com>
2026-06-26 17:29:06 +0300

    Advancement on the server

--- reallyBasicThings/projects101/lowLevelStuff/httpServer.c
+++ reallyBasicThings/projects101/lowLevelStuff/httpServer.c
@@ -1,4 +1,3 @@
-#include <netinet/in.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
@@ -6,6 +5,8 @@
 //These ones here because Beej said so (and probably to get getaddrinfo())
 #include <netdb.h> 
 #include <sys/types.h>
+
+#define BACKLOG 10 //this is not an overkill right?
 
 /*
     And here we are...I am diving head-first into networking as I code a fucking HTTP server...no idea what a HTTP server is for now...but I'll eventually learn on the way lol
@@ -39,25 +40,6 @@ int main(void)
                 Guess(may be way off) sa is short for socket address sin is for socket_internet? and sun for socket unix family...fuck...
         
     */
-    int fd = socket(AF_INET,SOCK_STREAM,0 ); //fd's short for file descriptor  0 and IPPROTO_TCP for tcp(7) stream sockets that explains why I have 0 as protocol it is TCP as I want
-    if(fd == -1) {perror("Socket, sucked it"); return(-1); }
-    
-    /*
-             .sin_family is always set to AF_INET.  This is required; in Linux 2.2 most networking functions return EINVAL when this setting is missing.  .sin_port contains the port in network  byte
-        order.   The  port numbers below 1024 are called privileged ports (or sometimes: reserved ports).  Only a privileged process (on Linux: a process that has the CAP_NET_BIND_SERVICE capa‐
-        bility in the user namespace governing its network namespace) may bind(2) to these sockets.  Note that the raw IPv4 protocol as such has no concept of a port, they are implemented  only
-        by higher protocols like tcp(7) and udp(7).
-
-        .sin_addr  is  the IP host address.  The .s_addr member of the in_addr(3type) structure contains the host interface address in network byte order.  in_addr(3type) should be assigned one
-        of the INADDR_* values (e.g., INADDR_LOOPBACK) using htonl(3) or set using the inet_aton(3), inet_addr(3), inet_makeaddr(3) library functions or directly with  the  name  resolver  (see
-        gethostbyname(3)).
-    
-        Am I using wrong family for binding&naming
-    
-
-    */
-
-
 
 
     struct addrinfo hint = { 0 }; //They use memset for somereason but I'll stick with this 'till I get seg-assult which honestly if they're only 0 initilizing I won't get since this is the same thing
@@ -70,8 +52,50 @@ int main(void)
     // bind(fd, const struct sockaddr *addr, socklen_t len) no clue what it does for now lol... 
     int res = getaddrinfo(NULL,"8080", &hint,&result); //8080 for non-administrive port (is non-administrive a word? I dunno you got the idea...I don't need to be admin to access basically )
 
+    int fd = socket(AF_INET,SOCK_STREAM,0 ); //fd's short for file descriptor  0 and IPPROTO_TCP for tcp(7) stream sockets that explains why I have 0 as protocol it is TCP as I want
+    if(fd == -1) {perror("Socket, sucked it"); return(-1); }
+
     int bindStatus = bind(fd, result->ai_addr,result->ai_addrlen); //   Upon successful completion, bind() shall return 0; otherwise, -1 shall be returned and errno set to indicate the error. (man bind page)
-    if (bindStatus == -1) {perror("Avoidant attachment issue, see a therapist ASAP\n\n"); return(-1); }
+    if (bindStatus == -1) {perror("Avoidant attachment issue, see a therapist ASAP\n"); return(-1); }
+
+    //Now since I've arrived to...yk acutal networking part I gotta listen() and accept() right...do I need fork()s? or is listen() a non-blocking function 
+
+    //Listening test 
+    int listenStatus = listen(fd,BACKLOG); 
+    /*
+        The  accept() system call is used with connection-based socket types (SOCK_STREAM, SOCK_SEQPACKET).  It extracts the first connection request on the queue of pending connections for the
+        listening socket, sockfd, creates a new connected socket, and returns a new file descriptor referring to that socket.  The newly created socket is not in the listening state.  
+        The original socket sockfd is unaffected by this call... so this explanation says me that I'll get a seperate fd (which is listeningStatus ig) after having a file waiting approvel from accept()
+        Does it fork the process or memcpy the old data? IDK...and I guess that's point of using a fucking API
+
+
+    */
+    if (listenStatus == -1) {perror("Port is like Beethoven after 1817 rn fr\n"); return(-1);}
+
+    struct sockaddr_storage clientAddr = { 0 };
+    socklen_t clientAddrSize = sizeof(clientAddr);
+
+    int clientFD = accept(fd, (struct sockaddr *)&clientAddr,&clientAddrSize); //As I thought blocks the shit 
+    if (clientFD == -1) { perror("Failed proposal attempt\n"); return(-1); } //Yeah I am running out of error jokes slowly
+    /*
+        This shit will be needed for myself to not soft-lock myself 
+
+    If no pending connections are present on the queue, and the socket is not marked as nonblocking, accept() blocks the caller until a connection is present.  If the socket is marked  non‐
+     blocking and no pending connections are present on the queue, accept() fails with the error EAGAIN or EWOULDBLOCK.
+
+     In  order to be notified of incoming connections on a socket, you can use select(2), poll(2), or epoll(7).  A readable event will be delivered when a new connection is attempted and you
+     may then call accept() to get a socket for that connection.  Alternatively, you can set the socket to deliver SIGIO when activity occurs on a socket; see socket(7) for details.
+
+     If flags is 0, then accept4() is the same as accept().  The following values can be bitwise ORed in flags to obtain different behavior:
+
+     SOCK_NONBLOCK   Set the O_NONBLOCK file status flag on the open file description (see open(2)) referred to by the new file descriptor.  Using this flag saves extra calls to fcntl(2)  to
+    achieve the same result.
+
+
+    
+    
+    */
+
 
     //since almost every server runs repeatetly 
     while (1) {
@@ -86,61 +110,3 @@ int main(void)
     return(0);
 }
 
-
-/*
-
-    Stole this from man 2 bind to learn how to fucking bind it's been fucking 20 minutes and I can't figure it out how to bind shit yet.
-
-
-     #include <err.h>  unnecessary in my case for now
-     #include <stdlib.h> 
-     #include <string.h>
-     #include <sys/socket.h>
-     #include <sys/un.h>
-     #include <unistd.h>
-
-     #define MY_SOCK_PATH "/somepath"
-     #define LISTEN_BACKLOG 50
-
-     int
-     main(void)
-     {
-         int                 sfd, cfd;
-         socklen_t           peer_addr_size;
-         struct sockaddr_un  my_addr, peer_addr;
-
-         sfd = socket(AF_UNIX, SOCK_STREAM, 0);
-         if (sfd == -1)
-             err(EXIT_FAILURE, "socket");
-
-         memset(&my_addr, 0, sizeof(my_addr));      I guess this is the 0 init 
-         my_addr.sun_family = AF_UNIX;              I'll probably need to use AF_INET instead in my case
-         strncpy(my_addr.sun_path, MY_SOCK_PATH,        Why do I need a path for my scocket ?
-                 sizeof(my_addr.sun_path) - 1);         That's the data part ig
-
-         if (bind(sfd, (struct sockaddr *) &my_addr,
-                  sizeof(my_addr)) == -1)
-             err(EXIT_FAILURE, "bind");
-
-            ==== UNRELATED FOR NOW ======
-
-         if (listen(sfd, LISTEN_BACKLOG) == -1)
-             err(EXIT_FAILURE, "listen");
-
-          Now we can accept incoming connections one
-            at a time using accept(2).  
-
-         peer_addr_size = sizeof(peer_addr);
-         cfd = accept(sfd, (struct sockaddr *) &peer_addr,
-                      &peer_addr_size);
-         if (cfd == -1)
-             err(EXIT_FAILURE, "accept");
-
-         if (close(sfd) == -1)
-             err(EXIT_FAILURE, "close");
-
-         if (unlink(MY_SOCK_PATH) == -1)
-             err(EXIT_FAILURE, "unlink");
-     }
-
-*/
