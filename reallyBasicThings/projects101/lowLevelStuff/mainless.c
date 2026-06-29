5379042303eea564cc097345839acce3bea0c157
neurmancer <m.arda.safak06@gmail.com>
2026-06-30 00:01:55 +0300

    Man I missed 00AM commit

--- reallyBasicThings/projects101/lowLevelStuff/mainless.c
+++ reallyBasicThings/projects101/lowLevelStuff/mainless.c
@@ -39,6 +39,11 @@ void _start() {
     //atexit(&cleanUP());
     _exit(0); //Basically a magnum to the temple with an empty bottle o' jack on the table (or kill -9 if you know)
     //return(0); would cause segfault (if not UB) since there is no crt0.o return TO...there is no return address on stack for _start 
+    /*
+        Explaination of said Segfault or UB (long version for the nerds Imma yap) _start is called by kernel itself via .global _start flag (you may see it in asm disassemblies)
+        and return or exit() returns value to _start but _start just sends a signal to kernel itself (via r7 or 7th register which is a special register) called SIGHUP and kernel knows 
+        that the program ended  (I think that's correct but sure lol)
+    */
 }
 
 /*
