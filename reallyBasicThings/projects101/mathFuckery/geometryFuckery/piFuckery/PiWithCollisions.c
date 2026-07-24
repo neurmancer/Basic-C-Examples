df79335b68161850d2c398614881ed898d34238c
neurmancer <m.arda.safak06@gmail.com>
2026-07-24 17:57:15 +0300

    I'll finish this evenutally

--- reallyBasicThings/projects101/mathFuckery/geometryFuckery/piFuckery/PiWithCollisions.c
+++ reallyBasicThings/projects101/mathFuckery/geometryFuckery/piFuckery/PiWithCollisions.c
@@ -9,13 +9,11 @@
         Question is 'how?'
 
 
-            Yk? let's leave it like that for a while...I've burnt out again
 */
 
 /* =========== INCLUDES ============== */
 
 #include <raylib.h>     //When raylib started being the first lib I include...I realized I have become a changed man...
-
 
 /* ================== DEFINES ================= */
 
@@ -28,10 +26,14 @@
 
 //Physics Object defines 
 
-#define SIZE_SMALL 130.0f       //They're square so... one side is enough
-#define SIZE_BIG 250.0f
+#define SIZE_SMALL 50.0f       //They're square so... one side is enough
+#define SIZE_BIG 100.0f
 
+#define BLOCK_AMOUNT 2 
 
+#define WALL_X (WIDTH/10)
+
+#define LINE_THICKNESS 3.5f
 
 /* ================== STRUCTS ================= */
 
@@ -42,6 +44,20 @@ typedef struct{
     //Yeah since I don't plan to move on Y axis one 1 dimensional position and vel would work Ig...
 }Block;
 
+typedef struct{
+
+    Vector2 p1;
+    Vector2 p2;
+}Line;
+
+typedef struct{
+
+    Block blocks[2];
+    
+    Line wall;
+    Line floor;     //I don't want globals in this project so setObject function gonna be a huge one and I don't wanna send every object pointer seperately
+
+}Objects;
 
 /* ================== GLOBALS ================= */
 
@@ -50,9 +66,14 @@ typedef struct{
 
 /* =========== FUNCTION PROTOTYPES ============ */
 
-void setBlocks(Block *bigOne, Block *lessBigOne);           
-void drawBlocks(Block blockArr[2]);
+void setObjects(Objects *objects);           
 
+// Drawing functions
+void drawBlocks(Block blockArr[BLOCK_AMOUNT]);
+void drawWallandFloor(Line wall,Line floor);
+
+
+//Shit required 
 int setUpWindow(void);
 
 /* ================== MAIN ================= */
@@ -62,13 +83,15 @@ int main(void)
 
     Block bully = { 0 };        //Big one 
     Block nerd = { 0 };         //Small one (if you can't tell duh)
-    
-    setBlocks(&bully, &nerd);
+    Line wall = { 0 };
+    Line floor = { 0 };
+
+    Objects objectArray = {bully,nerd,wall,floor};
 
 
-    Block blocks[] = {bully,nerd};
-    
-    
+    setObjects(&objectArray);       //Set object function takes blocks[0] as big one DON'T FORGET
+
+
 
 
     if (setUpWindow() == -1) {
@@ -81,7 +104,8 @@ int main(void)
 
         BeginDrawing();
         ClearBackground(BLACK);
-        drawBlocks(blocks);
+        drawWallandFloor(objectArray.wall,objectArray.floor);
+        drawBlocks(objectArray.blocks);
         EndDrawing();
     }
 
@@ -99,28 +123,48 @@ int setUpWindow(void)
         return(-1);
     }
     SetTargetFPS(FPS);
-    
+
     return(0);
 }
 
 
-void setBlocks(Block *bigOne, Block *lessBigOne)
+void setObjects(Objects *object)
 {
-    bigOne->pos = (Vector2){WIDTH/3,HEIGHT/4};       //Lol I learned what the cast looking as thing is btw it's a C compound literal  
-    bigOne->vx = -0.5f;      //Towards left  (I lowkey really started to type like a corpo but better than forcing jokes)
-    bigOne->m = 100.0f;
-    bigOne->edgeSize = SIZE_BIG;
+    object->blocks[0].pos = (Vector2){WIDTH/3,HEIGHT/2};       //Lol I learned what the cast looking as thing is btw it's a C compound literal  
+    object->blocks[0].vx = -0.5f;      //Towards left  (I lowkey really started to type like a corpo but better than forcing jokes)
+    object->blocks[0].m = 100.0f;
+    object->blocks[0].edgeSize = SIZE_BIG;
 
     float offset = SIZE_BIG-SIZE_SMALL;
-    lessBigOne->pos = (Vector2){(2*WIDTH)/3,(HEIGHT/4)+offset};
-    lessBigOne->vx = 0.0f;  
-    lessBigOne->m = 10.0f;
-    lessBigOne->edgeSize = SIZE_SMALL;
+    object->blocks[1].pos = (Vector2){(2*WIDTH)/3,(HEIGHT/2)+offset};
+    object->blocks[1].vx = 0.0f;  
+    object->blocks[1].m = 10.0f;
+    object->blocks[1].edgeSize = SIZE_SMALL;
+
+    object->wall.p1.x = WALL_X; 
+    object->wall.p1.y = HEIGHT/10;
+
+    object->wall.p2.x = WALL_X; 
+    object->wall.p2.y = (HEIGHT/2)+offset+SIZE_SMALL;
+
+    object->floor.p1.x = WALL_X;
+    object->floor.p1.y = (HEIGHT/2)+offset+SIZE_SMALL;
+
+    object->floor.p2.x = WIDTH/1.05f;
+    object->floor.p2.y = (HEIGHT/2)+offset+SIZE_SMALL;
+
 }
 
-void drawBlocks(Block blockArr[2])
+void drawBlocks(Block blockArr[BLOCK_AMOUNT])
 {
     for (int i = 0;i < 2;i++) {
         DrawRectangle(blockArr[i].pos.x,blockArr[i].pos.y, blockArr[i].edgeSize , blockArr[i].edgeSize, WHITE);
     }
+}
+
+void drawWallandFloor(Line wall,Line floor)
+{
+    DrawLineEx(wall.p1,wall.p2,LINE_THICKNESS, WHITE);
+    DrawLineEx(floor.p1,floor.p2,LINE_THICKNESS, WHITE);
+    
 }
