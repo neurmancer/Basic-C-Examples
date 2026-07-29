a701afe4f6da035925a0407f06fced0469c3adfe
neurmancer <m.arda.safak06@gmail.com>
2026-07-29 20:05:25 +0300

    Fuck I forgor

--- reallyBasicThings/projects101/mathFuckery/geometryFuckery/piFuckery/ramanujanFuckery/ramanujanThingy.c
+++ reallyBasicThings/projects101/mathFuckery/geometryFuckery/piFuckery/ramanujanFuckery/ramanujanThingy.c
@@ -26,7 +26,7 @@ int main(void)
     bigFloatZero(&bF);
 
     double x = sqrty(8)/9801; //The constant multiplier before Sigma notation in the equation
-
+    //I realized I didn't implement a printBigNum function so...I'll be going back that brb...
     for (int k = 0; k < 1000;k++) {
         /*And that's where my brain gave up...*/
     }
@@ -37,8 +37,8 @@ int main(void)
 
 double sqrty(unsigned int val)
 {
-    if (val == 0) { return(0.0f); }
-    if (val == 1) { return(1.0f); }
+    if (val == 0) { return(0.0l); }
+    if (val == 1) { return(1.0l); }
     
     int msb = 0;
     unsigned int temp = val;
@@ -47,9 +47,9 @@ double sqrty(unsigned int val)
     int expon = msb/2;
 
     double z = (double) (1u << expon);    
-    for (uint8_t i = 0; i < 8; i++) {  
+    for (uint8_t i = 0; i < 6; i++) {  
 
-        z = (z + val / z) * 0.5f;
+        z = (z + val / z) * 0.5l;
     }
     
     return(z);
