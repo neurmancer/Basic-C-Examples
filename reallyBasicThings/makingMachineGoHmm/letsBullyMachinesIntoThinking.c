45c7a0b54abc8bb432916477f9d06f9df3144b57
neurmancer <m.arda.safak06@gmail.com>
2026-07-29 19:05:10 +0300

    Yup my brain cell growing strong

--- reallyBasicThings/makingMachineGoHmm/letsBullyMachinesIntoThinking.c
+++ reallyBasicThings/makingMachineGoHmm/letsBullyMachinesIntoThinking.c
@@ -4,6 +4,7 @@
 
         Long version: We're trying to build a Machine Learning algorithm (probably it's backpropagation and shit) so I can split labors with it lulz
     
+        the unofficial title for this file's training my last brain cell
 
         Since this shit is new to me I am following the path of tsoding daily ML playlist
 */
@@ -14,7 +15,7 @@
 #include <unistd.h>
 
 float randFloat(void);
-float cost(float w);
+float cost(float w, float b);
 
 float train[][2] = {
     {0, 0},
@@ -32,12 +33,20 @@ int main(void)
     //Y = x*w input times paramter thingy
     float rate = 1e-3; //Learining rate for the machine(Honestly it would be fucking easier to send it to University bruh)
     float eps = 1e-3;   //Time to auotmate this with more calculus!!!
+    float bias = 5.0f;
 
-    float w = randFloat()*5.0f;
-    float dCost = (cost(w+eps)-cost(w))/eps;    //You 'member the definition of derivative right? That's kinda it lol you can call it just slope, finite differance etc.
-    w -= dCost*rate;
+    float w = randFloat()*15.0f;
 
+    for (size_t i = 0;i < 1000000; i++) {
+        float c = cost(w, bias);
+        float dW = (cost(w+eps,bias)-c)/eps;    //You 'member the definition of derivative right? That's kinda it lol you can call it just slope, finite differance etc.
+        float dB = (cost(w,bias+eps)-c)/eps;
+        w -= dW*rate;
+        bias -= rate*dB;
+        printf("cost: %f\tW:%f\tB:%f\n ",cost(w, bias),w,bias);
+    }   //Damn it gets smarter
 
+    printf("%f\n",w);
     
 
     return(0);
@@ -48,21 +57,21 @@ float randFloat(void)
     return((float)rand() / RAND_MAX);
 }
 
-float cost(float w)
+float cost(float w,float b)
 {   
-    float result;
+    float result = 0.0f;
     for(size_t i = 0;i < DATA_SIZE; i++)
     {
         float x = train[i][0];
-        float y = x*w;  //w is the tuning paramter type shit dunno yet...
-        float offset = y- train[i][1];
-        result = offset*offset;
+        float y = x*w + b;  //w is the tuning paramter type shit dunno yet...
+        float offset = y - train[i][1];
+        result += offset*offset;
             /*
             Two reasons to use offset^2 
             First = negative handling 
             Second = it gets huge. fast.AF. No mistake has forgiven.
         */
-        printf("Actual Y:%f\t expected Y: %f\n",y, train[i][1]);
+        //printf("Actual Y:%f\t expected Y: %f\n",y, train[i][1]);
     }    
     
     result /= (float)DATA_SIZE;
