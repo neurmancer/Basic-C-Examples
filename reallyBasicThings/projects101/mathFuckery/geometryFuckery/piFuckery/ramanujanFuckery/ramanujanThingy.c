4fdbad25962e9be6c3dc9d232376665356b20a44
neurmancer <m.arda.safak06@gmail.com>
2026-07-29 19:28:04 +0300

    Template for future use

---
+++ reallyBasicThings/projects101/mathFuckery/geometryFuckery/piFuckery/ramanujanFuckery/ramanujanThingy.c
@@ -0,0 +1,25 @@
+/*
+
+    Luckly this won't be a raylib project but unluckly that will be a me project where I'll use my own fft to multiply shit in my own big num lib to 
+    build this so this is the voltran of my side-quests anways...
+
+    What's today's fuckery? MORE PI GAMBA...
+    
+    Honestly? Math looks like a fucking hell to go and I highly recommend you to use the given Makefile 
+    this file is a tempalte for later use for now
+*/
+
+#include "bignums.h"
+
+int main(void)
+{
+
+    BigInt bigInt = { 0 };
+    bigIntZero(&bigInt);
+
+    BigFloat bF = { 0 };
+    bigFloatZero(&bF);
+
+    
+    return(0);
+}
