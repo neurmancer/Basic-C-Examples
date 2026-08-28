6d82d4fbbbbb32513db36270a037f668d2f5605b
neurmancer <m.arda.safak06@gmail.com>
2026-08-28 21:38:20 +0300

    IT'S GETTING ~~BETTER~~

--- reallyBasicThings/retroShit/breakout.c
+++ reallyBasicThings/retroShit/breakout.c
@@ -17,6 +17,10 @@
             2- Paddle movment
             3- Brick offsets and drawing shit
             4- Collision detection (Hardest part, it seems)
+                Since raylib's collision just answers the 'did collision happen' and I need the 'which' part more so AABB collision it is...
+                Checking balls position for each brick is another pain in the ass so we'll optimize that shit 
+
+
             5- Ball movement and shit
 
 
@@ -27,6 +31,9 @@
 /* ================== INCLUDES ===================== */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 //External Headers 
 #include <raylib.h>
 
@@ -50,12 +57,42 @@
 
 //Game thingies
 
-#define BRICK_ROWS 4
-#define BRICK_COLUMNS 14
+#define BRICK_ROWS 6
+#define BRICK_COLUMNS 12
 
+//Colors (using the same colors from gravityThingy.c)
 
-static const float BRICK_LENGTH = WIDTH/16;
-static const float BRICK_HEIGHT = HEIGHT/32;
+#define SKY_BLUE        CLITERAL(Color){  79, 195, 247, 255 }  // #4FC3F7
+#define AZURE           CLITERAL(Color){  41, 182, 246, 255 }  // #29B6F6
+#define ELECTRIC_CYAN   CLITERAL(Color){   0, 229, 255, 255 }  // #00E5FF
+#define TURQUOISE       CLITERAL(Color){  38, 198, 218, 255 }  // #26C6DA
+#define NEON_GREEN      CLITERAL(Color){   0, 230, 118, 255 }  // #00E676
+#define EMERALD         CLITERAL(Color){ 102, 187, 106, 255 }  // #66BB6A
+#define LIME_GREEN      CLITERAL(Color){ 156, 204, 101, 255 }  // #9CCC65
+#define YELLOW_GREEN    CLITERAL(Color){ 212, 225,  87, 255 }  // #D4E157
+#define BRIGHT_YELLOW   CLITERAL(Color){ 255, 235,  59, 255 }  // #FFEB3B
+#define GOLDY            CLITERAL(Color){ 255, 213,  79, 255 }  // #FFD54F
+#define AMBER           CLITERAL(Color){ 255, 183,  77, 255 }  // #FFB74D
+#define ORANGEY          CLITERAL(Color){ 255, 152,   0, 255 }  // #FF9800
+#define CORAL           CLITERAL(Color){ 255, 112,  67, 255 }  // #FF7043
+#define DEEP_ORANGE     CLITERAL(Color){ 244,  81,  30, 255 }  // #F4511E
+#define REDISH             CLITERAL(Color){ 239,  83,  80, 255 }  // #EF5350
+#define MAGENTA_DUH         CLITERAL(Color){ 233,  30,  99, 255 }  // #E91E63
+#define PINKY            CLITERAL(Color){ 240,  98, 146, 255 }  // #F06292
+#define HOT_PINK        CLITERAL(Color){ 236,  64, 122, 255 }  // #EC407A
+#define SHE_LOVES_PURPLE          CLITERAL(Color){ 171,  71, 188, 255 }  // #AB47BC
+#define SO_DO_I          CLITERAL(Color){ 126,  87, 194, 255 }  // #7E57C2
+#define INDIGO          CLITERAL(Color){  92, 107, 192, 255 }  // #5C6BC0
+#define BLUE_GRAY       CLITERAL(Color){ 144, 164, 174, 255 }  // #90A4AE
+#define SILVER          CLITERAL(Color){ 207, 216, 220, 255 }  // #CFD8DC
+#define RED_AF          CLITERAL(Color){  41,   3,   0, 255 }  // #290300
+
+#define COLOR_ARR_SIZE 24
+
+static const float BRICK_LENGTH = WIDTH/14; //That's gonna be used as left-right offset too  
+static const float BRICK_HEIGHT = HEIGHT/32;    //That might differ
+static const float Y_OFFSET = HEIGHT/10;
+
 //Why static? 'cuz scope thingy is a bitch...(not like I use multiple files but what if I Do?)
 
 /* =================== OBJECTS ========================= */
@@ -66,12 +103,18 @@ typedef struct{
     Vector2 size;
 
     Color color;
+
+    int isDestroyed;
     int powerUp;
+
 }Brick;
 
 typedef struct{
 
     Vector2 pos;
+    Vector2 vel;
+    Vector2 accel;  //For future power-ups
+
     float radius;
 
     Color color;
@@ -80,13 +123,17 @@ typedef struct{
 typedef struct{
 
     Vector2 pos;
-    Vector2 vel;
-    Vector2 accel;
-
     Vector2 size;
     Color color;
 
 }Paddle;
+
+typedef struct{
+    int remainingLives;
+    int score;
+
+}GameState;
+
 
 typedef struct{
 
@@ -96,13 +143,26 @@ typedef struct{
 
     Brick bricks[BRICK_ROWS][BRICK_COLUMNS];
 
+    GameState gameState;
 }Objects;
 
 typedef struct{
 
     //I'll try to make this configurable 'cuz why not but not now...
-    
 
+    Vector2 paddleSize;    
+    float paddleSpeed;
+
+    Color paddleColor; 
+
+    Vector2 ballVel;
+    Vector2 ballAccel;
+
+    float ballRadius;
+    Color ballColor;
+   
+    int scorePerBrick;
+    int playerLives;
 }Config;
 
 /* ================ (Hopefully Not) GLOBALS ================ */
@@ -111,8 +171,11 @@ typedef struct{
 
 /* ================= FUNCTION PROTOTYPES ==================== */
 
+void drawObjects(Objects *objs);
+void setObjects(Objects *objs, Config *cfg);
 
 int setupEnv(void);
+Color randColor(void);
 
 
 /* ============ MAIN ============= */
@@ -121,16 +184,36 @@ int main(void)
 {
 
     printf("C'mon I have to start with a printf right\n");
+    srand(time(NULL));
+
     if(setupEnv()) { printf("We fucked\n"); return(-53); }
 
 
     Objects objs = { 0 };
+    
+    Config cfg = {
+        .paddleSize = (Vector2){WIDTH/5, HEIGHT/50},
+        .paddleSpeed = 1,   //Pixel/dT
+        .paddleColor = WHITE,
+
+        .ballRadius = WIDTH/60,
+        .ballVel = 1,
+        .ballAccel = 0,
+        .ballColor = WHITE,
+    
+        .playerLives = 3,
+        .scorePerBrick = 100,
+    };
+    printf("Og debug tool\n");
+    setObjects(&objs, &cfg);
+    printf("Test\n");
 
     while (!WindowShouldClose()) {
         if (IsKeyPressed(KEY_ESCAPE)) { break;}
 
         BeginDrawing();
         ClearBackground(BLACK);
+        drawObjects(&objs);
         EndDrawing();
     }
     
@@ -159,4 +242,89 @@ int setupEnv(void)
 
 
     return(0);
+}
+
+Color randColor(void)
+{
+    static const Color COLOR_PALETTE[COLOR_ARR_SIZE] = {
+    SKY_BLUE,
+    AZURE,
+    ELECTRIC_CYAN,
+    TURQUOISE,
+    NEON_GREEN,
+    EMERALD,
+    LIME_GREEN,
+    YELLOW_GREEN,
+    BRIGHT_YELLOW,
+    GOLDY,
+    AMBER,
+    ORANGEY,
+    CORAL,
+    DEEP_ORANGE,
+    REDISH,
+    MAGENTA_DUH,
+    PINKY,
+    HOT_PINK,
+    SHE_LOVES_PURPLE,
+    SO_DO_I,
+    INDIGO,
+    BLUE_GRAY,
+    SILVER,
+    RED_AF
+    };
+
+    return(COLOR_PALETTE[rand()%COLOR_ARR_SIZE]);
+}
+
+
+void setObjects(Objects *objs, Config *cfg)
+{
+    
+    //Paddle Shit
+    objs->paddle.size = cfg->paddleSize;
+    objs->paddle.pos = (Vector2){((WIDTH-objs->paddle.size.x)/2), 24*HEIGHT/25 };
+    objs->paddle.color = cfg->paddleColor;
+
+    //Ball shit
+    objs->ball.radius = cfg->ballRadius;
+    objs->ball.pos.x = objs->paddle.pos.x + (objs->paddle.size.x/2);
+    objs->ball.pos.y = objs->paddle.pos.y - objs->ball.radius;
+    objs->ball.vel = cfg->ballVel;
+    objs->ball.accel = cfg->ballAccel;
+    objs->ball.color = cfg->ballColor;
+
+    objs->gameState = (GameState){cfg->playerLives, cfg->scorePerBrick};
+    
+    for (size_t i = 0;i < BRICK_COLUMNS; i++) {
+        for (size_t j = 0; j < BRICK_ROWS; j++) {
+            objs->bricks[j][i].pos = (Vector2){(i+1)*BRICK_LENGTH, (j*BRICK_HEIGHT)+Y_OFFSET};
+            objs->bricks[j][i].size = (Vector2){BRICK_LENGTH, BRICK_HEIGHT};
+            objs->bricks[j][i].color = randColor();
+            objs->bricks[j][i].powerUp = -1;
+            objs->bricks[j][i].isDestroyed = 0;
+        }
+    }
+    //I guess every fucking thing is settled but I HIGHLY DOUBT THAT THIS IS THE FUCKING RIGHT WAY
+    return;
+}
+
+
+void drawObjects(Objects *objs)
+{
+
+    for (size_t i = 0; i < BRICK_ROWS; i++) {
+        for (size_t j = 0; j < BRICK_COLUMNS; j++) {
+            if (objs->bricks[i][j].isDestroyed) {
+                continue;
+            }
+            DrawRectangleV(objs->bricks[i][j].pos, objs->bricks[i][j].size, objs->bricks[i][j].color);
+            DrawRectangleLinesEx(
+            (Rectangle){objs->bricks[i][j].pos.x,objs->bricks[i][j].pos.y, objs->bricks[i][j].size.x, objs->bricks[i][j].size.y}, 
+            3.5f, 
+            BLACK);
+        }
+    }
+    DrawRectangleV(objs->paddle.pos, objs->paddle.size, objs->paddle.color);
+    DrawCircleV(objs->ball.pos, objs->ball.radius , objs->ball.color);
+
 }
