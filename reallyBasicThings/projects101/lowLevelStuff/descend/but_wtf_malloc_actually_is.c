a43caae4de3c8fd4734201d56b94387add0180c8
neurmancer <m.arda.safak06@gmail.com>
2026-09-04 11:33:12 +0300

    SBRK SAID YUP!

---
+++ reallyBasicThings/projects101/lowLevelStuff/descend/but_wtf_malloc_actually_is.c
@@ -0,0 +1,36 @@
+/*
+*
+*
+*
+*       'Sup? What's today's low-level fuckery? wriitng malloc using ancient scrolls (sbrk) before building a VM and understand virtual memory to do the same shit using 
+*       mmap() 
+*
+*       This is a template for now and this shit has prerequiests(or how tf u spell it)
+*
+*       So here are shits to know:
+*
+*       0-Wtf are pointers
+*       1-Basic struct usage
+*       2-And esentials of DSA for doubly linked lists (I guess...haven't started writing a single line of code yet) 
+*
+*
+*
+* */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+int main(void)
+{
+
+
+
+  return(0);
+}
+
+
+
